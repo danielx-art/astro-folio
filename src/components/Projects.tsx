e1ca1c25204c0b035a9b77281f6a472468a0a97a
danielx-art <metacarpo10@gmail.com>
2023-03-16 21:31:42 -0300

    projects page styling and dictionary

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,32 +1,43 @@
 import { getCollection } from "astro:content";
 const projects = await getCollection("projects");
-//const basePath = "https://danielx-art.github.io/astro-folio"
 
-export const Projects: React.FC<{ basePath: string }> = ({ basePath }) => {
+export const Projects: React.FC<{ basePath: string; lang: string }> = ({
+  basePath,
+  lang,
+}) => {
   return (
-    <div className="w-full h-full p-4 text-white">
+    <div className="w-full h-full sm:p-4 text-white">
       {projects.map((project, index) => {
         return (
           <div
-            className="p-8 w-full h-fit flex flex-row justify-start bg-slate-700"
+            className="p-8 w-full h-fit flex gap-8 flex-col-reverse sm:flex-row justify-center items-center "
             key={project.id}
           >
-            <div className="w-3/5 p-4 h-100 flex flex-col justify-center gap-4">
-              <h2 className="text-lg">{project.data.title}</h2>
+            <div className="sm:w-3/5 p-4 sm:h-100 flex flex-col justify-center gap-4">
+              <h2 className="text-lg font-bold">{project.data.title[lang]}</h2>
 
-              <div className="">{project.data.description}</div>
+              <div className="">{project.data.description[lang]}</div>
 
-              <div className="w-fit flex flex-row gap-6">
+              <div className="w-fit flex flex-row gap-6 self-center sm:mt-8">
                 {project.data.techs.map((tech, techIndex) => (
-                  <img
-                    src={`${basePath}/tech-logos/${tech}.png`}
-                    className="w-8 aspect-square object-contain"
-                    key={techIndex}
-                  ></img>
+                  <div className="group relative">
+                    <span
+                      className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto"
+                    >
+                      {tech}
+                    </span>
+                    <img
+                      src={`${basePath}/tech-logos/${tech}.png`}
+                      className="w-8 aspect-square object-contain"
+                      key={techIndex}
+                      alt={tech}
+                    ></img>
+                  </div>
                 ))}
               </div>
             </div>
-            <div className="w-1/2 relative">
+            <div className="w-1/2 relative outline-dashed outline-2 outline-pallete4c rounded-lg">
               <img
                 src={`${basePath}/projects/thumbs/${index + 1}.png`}
                 className="w-full aspect-square object-contain m-0 p-0"
