24cc85cfe96128fe006b15dbfc3b8b235cdabde8
Daniel Rangel Guedes <metacarpo10gmail.com>
2023-10-04 21:54:33 -0300

    minor

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,16 +23,16 @@ export const Hero: React.FC<{ lang: string; basePath: string }> = ({
   return (
     <>
       <div className="w-full h-full absolute flex flex-col justify-center px-12 z-10">
-        <div className="select-none text-black text-opacity-100 text-justify mx-auto w-full sm:w-1/2 text-lg font-fira flex flex-row flex-wrap justify-center mt-auto">
-          {text[lang][0].split(" ").map((word) => (
-            <div className="border-none w-fit h-fit bg-white bg-opacity-100 px-1 my-1">
+        <div className="select-none text-black text-opacity-100 mx-auto sm:ml-[10%] sm:mr-auto w-full sm:w-1/3 text-lg font-fira flex flex-row flex-wrap justify-center sm:justify-start mt-auto">
+          {text[lang][0].split(" ").map((word, index) => (
+            <div className="border-none w-fit h-fit bg-white bg-opacity-100 px-1 my-1" key={'word'+index}>
               {word}
             </div>
           ))}
         </div>
         <div className="flex flex-row flex-wrap my-4 gap-4 justify-center text-lg font-fira mx-auto select-none">
           <a
-            className="flex flex-col justify-center items-center select-none hover:select-none text-white hover:text-pallete4c outline-2 outline-dashed outline-white hover:outline-2 hover:outline-pallete4c shadow-none cursor-pointer min-w-32 px-8 py-16 rounded-full"
+            className="text-xl flex flex-col justify-center items-center select-none hover:select-none text-white hover:text-pallete4c outline-2 outline-dashed outline-white hover:outline-2 hover:outline-pallete4c shadow-none cursor-pointer min-w-max px-20 py-16 rounded-full"
             href={`${basePath}/${lang}/projects`}
           >
             {/* <div className="text-8xl">
