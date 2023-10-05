5cf369467a7f7e1b7acd94fad2672b3db29a7168
Daniel Rangel Guedes <metacarpo10gmail.com>
2023-10-05 16:32:07 -0300

    changed some layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,17 +22,17 @@ export const Hero: React.FC<{ lang: string; basePath: string }> = ({
 }) => {
   return (
     <>
-      <div className="w-full h-full absolute flex flex-col justify-center px-12 z-10">
-        <div className="select-none text-black text-opacity-100 mx-auto sm:ml-[10%] sm:mr-auto w-full sm:w-1/3 text-lg font-fira flex flex-row flex-wrap justify-center sm:justify-start mt-auto">
+      <div className="w-full h-full absolute flex flex-col justify-center sm:items-center px-12 z-10">
+        <div className="select-none text-black text-opacity-100 mx-auto w-full sm:w-1/3 text-lg font-fira flex flex-row flex-wrap justify-center sm:justify-end mt-auto sm:h-fit sm:-translate-x-1/2 sm:pr-2">
           {text[lang][0].split(" ").map((word, index) => (
             <div className="border-none w-fit h-fit bg-white bg-opacity-100 px-1 my-1" key={'word'+index}>
               {word}
             </div>
           ))}
         </div>
-        <div className="flex flex-row flex-wrap my-4 gap-4 justify-center text-lg font-fira mx-auto select-none">
+        <div className=" flex flex-row flex-wrap mb-4 mt-12 gap-4 justify-center w-fit mx-auto select-none sm:h-fit sm:translate-x-1/2 sm:pl-2">
           <a
-            className="text-xl flex flex-col justify-center items-center select-none hover:select-none text-white hover:text-pallete4c outline-2 outline-dashed outline-white hover:outline-2 hover:outline-pallete4c shadow-none cursor-pointer min-w-max px-8 py-8 sm:px-20 sm:py-12 rounded-full"
+            className=" rounded-sm font-fira sm:text-4xl text-lg flex flex-col justify-center items-center select-none hover:select-none text-pallete4a hover:text-pallete4c  bg-pallete4c hover:bg-transparent hover:transition-all outline-2 outline-dashed hover:outline-2 hover:outline-pallete4c shadow-none cursor-pointer min-w-max px-8 py-8 sm:px-12 sm:py-12"
             href={`${basePath}/${lang}/projects`}
           >
             {/* <div className="text-8xl">
--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -13,14 +13,14 @@ export const ProjectsPage: React.FC<TProjectsPage> = ({
 }) => {
   return (
     <div className="bg-pallete4a w-full min-h-full sm:p-4 text-white sm:mt-12">
-      <div className="flex flex-wrap gap-8 justify-items-center items-center">
+      <div className="flex flex-wrap gap-4 items-center sm:px-[10%]">
         {content.map((project, index) => {
           return (
             <div
               key={project.id}
-              className="w-4/5 mx-auto sm:max-w-md flex flex-nowrap flex-col justify-center items-center"
+              className="w-4/5 mx-auto sm:max-w-md flex flex-nowrap flex-col justify-center items-center sm:bg-transparent bg-[rgba(255,255,255,0.02)] my-4 pb-8 rounded-lg"
             >
-              <div className="relative my-4 mx-auto sm:max-w-sm outline-double outline-1 outline-pallete4c rounded-lg overflow-hidden">
+              <div className="relative mb-4 mx-auto sm:max-w-sm outline-double outline-1 outline-pallete4c rounded-lg overflow-hidden">
                 <a href={project.data.link ? project.data.link : undefined}>
                   <img
                     src={`${basePath}/projects/thumbs/${index + 1}.png`}
@@ -34,10 +34,10 @@ export const ProjectsPage: React.FC<TProjectsPage> = ({
                   githubLink={project.data.github}
                 />
               )}
-              <div className="my-4 text-2xl font-bold text-pallete4c">
+              <div className="my-4 text-2xl font-bold text-pallete4c sm:self-start sm:ml-8 px-4">
                 {project.data.title}
               </div>
-              <div className="my-4 text-md text-center">
+              <div className="mb-4 text-md text-center sm:text-end sm:px-8 px-4">
                 {project.data.description}
               </div>
               <ProjectTechs techs={project.data.techs} basePath={basePath} />
