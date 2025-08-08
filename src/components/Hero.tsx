00266f82859efe078f1b0ac99ebce8c78a0b19d5
Daniel <daniel.rangel.guedes@gmail.com>
2025-08-08 01:05:19 -0300

    refactor: colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ export const Hero: React.FC<HeroProps> = ({ lang, basePath }) => {
   return (
     <>
       <div className="absolute my-auto p-2 inset-0 sm:inset-[10%] lg:inset-x-[20%] flex flex-col items-center justify-center h-fit max-h-screen z-10 overflow-show gap-4">
-        <div className="flex flex-col px-16 sm:px-0 sm:self-center select-none text-white font-fira">
+        <div className="flex flex-col px-16 sm:px-0 sm:self-center select-none text-text font-fira">
           <span className="font-bold text-3xl">{currentContent.title}</span>
           <span className="font-bold text-xl">{currentContent.subtitle}</span>
           <span className="flex flex-wrap font-normal pt-4 sm:text-justify">
@@ -42,14 +42,14 @@ export const Hero: React.FC<HeroProps> = ({ lang, basePath }) => {
         </div>
         <div className="flex justify-center sm:w-full select-none">
           <a
-            className="flex flex-nowrap px-4 py-2 rounded-sm font-fira font-bold text-lg  text-pallete4a hover:text-pallete4c bg-pallete4c hover:bg-transparent hover:outline-pallete4c outline-dashed outline-2 outline-transparent hover:transition-all shadow-lg cursor-pointer focus:outline-offset-2 focus:outline focus:outline-1 focus:outline-white     "
+            className="flex flex-nowrap px-4 py-2 rounded-sm font-fira font-bold text-lg  text-background hover:text-primary bg-primary hover:bg-transparent hover:outline-primary outline-dashed outline-2 outline-transparent hover:transition-all shadow-lg cursor-pointer focus:outline-offset-2 focus:outline focus:outline-1 focus:outline-text     "
             href={`${basePath}/${lang}/projects`}
             aria-label={currentContent.buttonText}
           >
             {currentContent.buttonText}
           </a>
         </div>
-        <div className="w-1/2 flex justify-center pt-4 border-t-2 border-pallete4c border-opacity-[.05] gap-4">
+        <div className="w-1/2 flex justify-center pt-4 border-t-2 border-primary/[0.05] gap-4">
           <SocialMediaNav />
         </div>
       </div>
